a8abb61ac9a4e434d8612269a50abdaf4ecb9de5
Fernando <kinggprobr@gmail.com>
2023-12-08 15:56:37 -0300

    0812

--- aula_09/ex8/main.c
+++ aula_09/ex8/main.c
@@ -11,27 +11,155 @@ exemplos mostram n = 5)
 #   # #   # ####   ####   #    # # 
 #####  ###  ##### #####   #     #  
 */
-
-int main () {
+int main() {
     int n;
-    int row = 0;
-    int col = 0;
-    printf("Digite um numero impar dentro de 2 a 20: ");
-    scanf("%d",&n);
-    
-        if(n % 2 == 0 || n < 2 || n > 20){
-            printf("Numero invalido");
-        } else{
+    int linha;
+    int coluna;
+    int meio;
+    int e;
+    int d;
 
-            while (col < n){
-                int row = 0;
-                for (row; row < n; row++) {
-                    printf("#");
-                }
-                printf("\n");
-                col++;
+    do {
+        do {
+            printf("Digite o valor de n (impar entre 3 e 19): ");
+            scanf("%d", &n);
+        } while (n < 3 || n > 19);
+    } while(n % 2 == 0);
+
+    // primeira forma: quadrado
+    for(linha = 0; linha < n; linha++)
+    {
+        if(linha == 0 || linha == (n-1))
+        {
+            for(coluna = 0; coluna < n; coluna++)
+            {
+                printf("#");
             }
-
         }
-    return 0;
+        else
+        {
+            printf("#");
+            for(coluna = 0; coluna < (n - 2); coluna++)
+            {
+                printf(" ");
+            }
+            printf("#");
+        }
+        printf("\n");
     }
+    printf("\n");
+
+    // segunda forma: quadrado com cantos arredondados
+    for(linha = 0; linha < n; linha++)
+    {
+        if(linha == 0 || linha == (n-1))
+        {
+            printf(" ");
+            for(coluna = 0; coluna < (n-2); coluna++)
+            {
+                printf("#");
+            }
+            printf(" ");
+        }
+        else
+        {
+            printf("#");
+            for(coluna = 0; coluna < (n - 2); coluna++)
+            {
+                printf(" ");
+            }
+            printf("#");
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // terceira forma: triangulo 1
+    for(linha = 0; linha < n; linha++)
+    {
+        for(coluna = 0; coluna < (linha + 1); coluna ++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // Quarta forma: triangulo 2
+    for(linha = 0; linha < n; linha++)
+    {
+        for(coluna = 0; coluna < (n - 1 - linha); coluna++)
+        {
+            printf(" ");
+        }
+        for(coluna = 0; coluna < (linha + 1); coluna ++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // Quinta forma: triangulo 3
+    for(linha = 0; linha < n; linha++)
+    {
+        for(coluna = 0; coluna < linha; coluna++)
+        {
+            printf(" ");
+        }
+        for(coluna = 0; coluna < (n - linha); coluna ++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // sexta forma: cruz
+    meio = n / 2;
+    for(linha = 0; linha < n; linha++){
+        if(linha == meio){
+            for(coluna = 0; coluna < n; coluna++){
+                printf("#");
+            }
+            printf("\n");
+        }
+        else {
+            for (coluna = 0; coluna < n; coluna++) {
+                if (coluna == meio) {
+                    printf("#");
+                } else {
+                    printf(" ");
+                }
+            }
+            printf("\n");
+        }
+    }
+    printf("\n");
+
+    // setima forma: losango
+    e = meio;
+    d = meio;
+    for(linha = 0; linha < n; linha++){
+        for(coluna = 0; coluna < n; coluna++){
+            if(coluna == e || coluna == d){
+                printf("#");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+        if(linha < meio){
+            e--;
+            d++;
+        }
+        else{
+            e++;
+            d--;
+        }
+    }
+    printf("\n");
+
+    return 0;
+}

ae92daaca2164417bce87a8ec857dcedcc39938c
Fernando Divino de Moraes Júnior <kinggprobr@gmail.com>
2024-08-20 22:24:46 -0300

    2008

--- 3° Semestre/Estruturas de Dados/Projetos/Pilha/main.c
+++ 3° Semestre/Estruturas de Dados/Projetos/Pilha/main.c
@@ -3,10 +3,15 @@
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
 
 int main (int argc, char *argv[]) {
     char exp[100] = "";
     char vec[100][20] = {'\0'};
+    int count_operadores = 0;
+    
+    char operadores[100] = {'\0'};
+    char numeros[100] = {'\0'};
 
     limpaVetor(vec, 100);
 
@@ -14,6 +19,26 @@ int main (int argc, char *argv[]) {
 
     stringParaVetor(exp, vec, 100);
 
-    printf("ou");
+    for(int i = 0; i < 100 && (strcmp(vec[i], "") !=  0); i++){
+        printf("%s\n", vec[i]);
+
+
+        if(strchr(vec[i], '(') != NULL){
+            count_operadores++;
+            printf("abre parenteses\n");
+            operadores[count_operadores] = vec[i];
+        }
+    }
+
+    // for (int i = 0; i < 100; i++){
+    //     printf("")
+    // }
+    
+
+
+
+
+    
+
     return 0;
 }

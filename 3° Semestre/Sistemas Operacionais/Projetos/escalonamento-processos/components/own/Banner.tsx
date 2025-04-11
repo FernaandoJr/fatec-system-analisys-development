3ee24024fcd48e9b625fb8812a02b11baab31d66
Fernando Divino de Moraes Junior <kinggprobr@gmail.com>
2025-04-11 12:30:44 -0300

    movendo projeto de escalonamento

--- 3° Semestre/Sistemas Operacionais/Projetos/escalonamento-processos/components/own/Banner.tsx
+++
@@ -1,8 +0,0 @@
-export default function Banner() {
-    return (
-        <div className="flex flex-col items-center justify-center gap-2">
-            <h1 className="text-title">TaskFlow</h1>
-            <p className="text-center">Simulador de algoritmos de escalonamento de processos utilizando Circular (Round-Robin) e Shortest Job First (SJF).</p>
-        </div>
-    )
-}

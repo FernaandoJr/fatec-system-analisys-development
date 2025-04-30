398c0a7508c5b62c86c5965dd3adef73c4f851e1
Fernando Divino de Moraes Junior <kinggprobr@gmail.com>
2025-04-30 12:54:02 -0300

    3004

---
+++ 4° Semestre/Programação Orientada a Objetos/Exercícios/Aula2904/ControleBancario/src/App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+import model.Conta;
+
+public class App {
+	public static void main(String[] args) {
+		Conta conta1 = new Conta();
+		Scanner sc = new Scanner(System.in);
+		try {
+			System.out.println("Digite um valor para deposito: ");
+			double valor = Double.parseDouble(sc.nextLine());
+			conta1.depositar(valor);
+			System.out.println("Digite um valor para saque: ");
+			valor = Double.parseDouble(sc.nextLine());
+			conta1.sacar(valor);
+			System.out.println("Saldo da conta: " + conta1.getSaldo());
+		} catch (NumberFormatException ex) {
+			System.out.println("Entrada inválida de dados");
+		} catch (Exception ex) {
+			System.out.println("Erro: " + ex.getMessage());
+		}
+
+	}
+}

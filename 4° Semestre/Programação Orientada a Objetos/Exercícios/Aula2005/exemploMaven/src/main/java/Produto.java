80214bc19149b10b28623996e3358885af41cc7f
Fernando Divino de Moraes Junior <kinggprobr@gmail.com>
2025-05-20 20:01:54 -0300

    2005

--- 4° Semestre/Programação Orientada a Objetos/Exercícios/Aula2005/exemploMaven/src/main/java/Produto.java
+++ 4° Semestre/Programação Orientada a Objetos/Exercícios/Aula2005/exemploMaven/src/main/java/Produto.java
@@ -1,4 +1,58 @@
-package PACKAGE_NAME;
-
 public class Produto {
+    private int idProduto;
+    private String nomeProduto;
+    private String descricaoProduto;
+    private double precoProduto;
+    private boolean ativo;
+
+
+    public Produto(int idProduto, String nomeProduto, String descricaoProduto, double precoProduto, boolean ativo) {
+        this.idProduto = idProduto;
+        this.nomeProduto = nomeProduto;
+        this.descricaoProduto = descricaoProduto;
+        this.precoProduto = precoProduto;
+        this.ativo = ativo;
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getDescricaoProduto() {
+        return descricaoProduto;
+    }
+
+    public void setDescricaoProduto(String descricaoProduto) {
+        this.descricaoProduto = descricaoProduto;
+    }
+
+    public double getPrecoProduto() {
+        return precoProduto;
+    }
+
+    public void setPrecoProduto(double precoProduto) {
+        this.precoProduto = precoProduto;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
 }
+
+
